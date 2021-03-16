9df8be54e91716b9c679a3f8db4bf2d3397a1ee0
isahajee <isahajee@gmail.com>
2021-03-16 03:30:27 +0000

    Add files via upload

--- CISC499/website.py
+++ CISC499/website.py
@@ -31,7 +31,36 @@ def upload_file():
 		except:
 			print("width is not a valid number")
 		
-	return redirect("/")
+	return redirect("/output.html")
+
+@app.route('/application.html')
+def wrapping():
+	return render_template("application.html")
+
+@app.route("/about.html")
+def about():
+	return render_template("about.html")
+
+@app.route("/contact.html")
+def contact():
+	return render_template("contact.html")
+
+@app.route("/help.html")
+def help():
+	return render_template("help.html")
+
+@app.route("/output.html")
+def output():
+	svg1 = open("anim1.svg").read()
+	svg2 = open("anim2.svg").read()
+	svg3 = open("anim3.svg").read()
+	svg4 = open("anim4.svg").read()
+	svgfinal = open("ex.svg").read()
+	return render_template("output.html", svg1 = svg1, svg2 = svg2, svg3 = svg3, svg4 = svg4, svgfinal = svgfinal)
+
+@app.route("/download.html", methods=['GET'])
+def download():
+	return send_file('ex.svg', as_attachment = True)
 
 if __name__ == "__main__":
 	app.run(debug=True)
